2e0cf4cad3e0d7e71a82b6ebdc267bf3cff17597
Wesley Schneider Aires <wesley.aires@cwi.com.br>
2022-12-18 17:22:34 -0300

    🎉 - Cria projeto com Spring Boot

---
+++ src/main/java/br/com/wesleyschneider/springbootprometheusgrafana/SpringBootPrometheusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package br.com.wesleyschneider.springbootprometheusgrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrometheusGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrometheusGrafanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wesleyschneider/springbootprometheusgrafana/SpringBootPrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wesleyschneider.springbootprometheusgrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPrometheusGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
